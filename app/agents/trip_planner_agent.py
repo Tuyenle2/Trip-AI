f5d4f8bbb21da44c1764ec3a5d0eee7bd7bd0edf
Lê Xuân Tuyên <utvanle113@gmail.com>
2026-04-10 10:53:40 +0700

    Update trip_planner_agent.py

--- app/agents/trip_planner_agent.py
+++ app/agents/trip_planner_agent.py
@@ -3,6 +3,7 @@ import os
 from datetime import datetime
 import pytz
 import certifi
+from pymongo import MongoClient  
 from langchain_community.utilities import SerpAPIWrapper
 from langchain_core.tools import Tool, tool
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
@@ -11,8 +12,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.messages import HumanMessage, SystemMessage
 from langgraph.graph import StateGraph, START, END, MessagesState
 from langgraph.prebuilt import ToolNode, tools_condition
-from motor.motor_asyncio import AsyncIOMotorClient
-from langgraph.checkpoint.mongodb.aio import AsyncMongoDBSaver
+from langgraph.checkpoint.mongodb import MongoDBSaver
+
 
 
 
@@ -29,8 +30,8 @@ class TripPlannerAgent:
         self._setup_tools()
         self._setup_llm()
         
-        self.client = AsyncIOMotorClient(self.mongodb_uri, tls=True, tlsCAFile=certifi.where())
-        self.memory = AsyncMongoDBSaver(self.client)
+        self.client = MongoClient(self.mongodb_uri, tls=True, tlsCAFile=certifi.where())
+        self.memory = MongoDBSaver(self.client)
 
         workflow = StateGraph(MessagesState)
         workflow.add_node("planner", self.planner_nod)
